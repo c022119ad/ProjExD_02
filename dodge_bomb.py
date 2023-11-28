7e9b239e451071ff6b87f1c55c0b75ed461f248a
c0b22119 <c0b22119ad@edu.teu.ac.jp>
2023-11-28 14:10:00 +0900

    爆弾Surfaceの追加

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import pygame as pg
 
@@ -11,18 +12,28 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bm_img = pg.Surface((20,20))  # 練習1透明のSurfaceを生成
+    pg.draw.circle(bm_img,(255,0,0),(10,10),10)  # 練習1赤い半径の円をdraw
+    bm_img.set_colorkey((0,0,0))
+    bm_rct = bm_img.get_rect()
+    bm_rct.centerx = random.randint(0,WIDTH)
+    bm_rct.centery = random.randint(0,HEIGHT)
+    
     clock = pg.time.Clock()
     tmr = 0
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-
+        
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bm_img,bm_rct)
+        
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(60)
 
 
 if __name__ == "__main__":
