2596e46c1fffa9db4e034efa72b900a48a23905f
c0b22119 <c0b22119ad@edu.teu.ac.jp>
2023-11-28 14:31:57 +0900

    こうかとんsurfaceをいどう

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,6 +5,13 @@ import pygame as pg
 
 WIDTH, HEIGHT = 1600, 900
 
+delta = {
+pg.K_UP:(0,-5),
+pg.K_DOWN:(0,+5),
+pg.K_LEFT:(-5,0),
+pg.K_RIGHT:(5,0)
+}  # 練習3 移動キーの作成
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -12,10 +19,12 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()  # 練習3 こうかとんSurfaceのrectを抽出
     bm_img = pg.Surface((20,20))  # 練習1透明のSurfaceを生成
     pg.draw.circle(bm_img,(255,0,0),(10,10),10)  # 練習1赤い半径の円をdraw
     bm_img.set_colorkey((0,0,0))
     bm_rct = bm_img.get_rect()
+    kk_rct.center = 900,400
     bm_rct.centerx = random.randint(0,WIDTH)
     bm_rct.centery = random.randint(0,HEIGHT)
     vx = 5
@@ -27,9 +36,16 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-            
+        key_lst = pg.key.get_pressed()
+        summove =[0,0]
+        for k,tpl in delta.items():
+            if key_lst[k]:  # 矢印キーが押されたら合計移動量を入れる
+                summove[0] += tpl[0]
+                summove[1] += tpl[1]
+        
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        kk_rct.move_ip(summove[0],summove[1])
+        screen.blit(kk_img, kk_rct)
         bm_rct.move_ip(vx,vy)  # 練習2 爆弾を動かす
         screen.blit(bm_img,bm_rct)
         
